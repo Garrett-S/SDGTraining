c45cfaf2b08a943b2015964ee9cf5871351c9123
Garrett-S <smithgar@oregonstate.edu>
2016-02-29 12:22:04 -0800

    Test commit from VS

---
+++ PeopleProTraining/PeopleProTraining/Models/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleProTraining.Models
+{
+    public class Building
+    {
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+    }
+}
---
+++ PeopleProTraining/PeopleProTraining/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleProTraining.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int RoomNumber { get; set; }
+        public string Building { get; set; }
+        public string Department { get; set; }
+        public string Title { get; set; }
+        public Decimal Salary { get; set; }
+
+        
+
+
+    }
+}
